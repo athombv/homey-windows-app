490b5ab23ceb6c77fe0ba7f5156d3ea5a5af56c3
Emile Nijssen <emile@athom.nl>
2021-01-09 21:34:47 +0100

    add Homey

---
+++ Homey/Form1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Homey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Resize += new System.EventHandler(this.Form_Resize);
+
+            webView.CreationProperties = new CoreWebView2CreationProperties
+            {
+                BrowserExecutableFolder = Path.Combine(Environment.CurrentDirectory, @"WebView2"),
+                UserDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"Athom", @"Homey")
+            };
+            webView.Size = this.ClientSize;
+            webView.Source = new Uri("https://my.homey.app");
+            webView.CoreWebView2Ready += WebView_CoreWebView2Ready;
+        }
+
+        private void WebView_CoreWebView2Ready(object sender, EventArgs e)
+        {
+            webView.CoreWebView2.DocumentTitleChanged += CoreWebView2_DocumentTitleChanged;
+        }
+
+        private void CoreWebView2_DocumentTitleChanged(object sender, object e)
+        {
+            this.Text = webView.CoreWebView2.DocumentTitle;
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView.Size = this.ClientSize;
+        }
+    }
+}
